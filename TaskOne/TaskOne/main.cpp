bb083aa6651e9b4c0e94b1c1c3f1dff5b4a3dfcc
Dmitry <84668584+ar-kinder@users.noreply.github.com>
2021-05-23 22:07:54 +0300

    первое задание
    вводится 2 целых числа и выводит
    1)умножение
    2)разницу
    3)сумму
    4)деление

---
+++ TaskOne/TaskOne/main.cpp
@@ -0,0 +1,63 @@
+//
+//  main.cpp
+//  TaskOne
+//
+//  Created by Дмитрий Зяблев on 23.05.21.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main5 ()
+{
+    int a;
+    int z;
+    cout << "Введите перкое целое число : ";
+    cin >> a;
+    cout << "Введите второе целое число : ";
+    cin >> z;
+    cout << a << "*" << z << "=" << a*z << endl;
+    cout << "a*z=" << a*z << endl;
+    cout << "a-z=" << a-z << endl;
+    cout << "a+z=" << a+z << endl;
+    cout << "a/z=" << a/z << endl;
+    return 0;
+}
+
+// Деление
+int main4 ()
+{ cout << "20/5=" << 20/5 << endl;
+    return 0;
+}
+// Разность
+int main3()
+{ cout << "20-5=" << 20-5 << endl;
+    return 0;
+}
+// "Сумма"
+int main2 ()
+{ cout << "20+5=" << 20+5 << endl;
+    return 0;
+}
+// "Умножение"
+int main1 ()
+{
+    cout << "20*5=" << 20*5 << endl;
+    return 0;
+}
+
+
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    
+    
+    
+    main5();
+    
+    
+    return 0;
+}
+
